e7c94a34c99045c302d6e4c03976d0d901f13d13
udhos <udhos>
2019-08-06 19:18:00 -0300

    Delete regional cluster.

--- 99-cluster-kill.sh
+++ 99-cluster-kill.sh
@@ -14,7 +14,15 @@ die() {
 
 cat <<__EOF__
 CLUSTER=$CLUSTER
+REGION=$REGION
+ZONE=$ZONE
 __EOF__
 
 msg run this command to delete the cluster:
-msg gcloud container clusters delete $CLUSTER
+
+if [ -n "$REGION" ]; then
+	msg gcloud container clusters delete $CLUSTER --region=$REGION
+else
+	[ -n "$ZONE" ] || die "missing env var ZONE"
+	msg gcloud container clusters delete $CLUSTER --zone=$ZONE
+fi
