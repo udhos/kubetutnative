1ca840a86ddff1f1af4bbbd10e68dddcff3d9c5a
udhos <udhos>
2019-08-06 16:59:39 -0300

    kubectl run is deprecated for deployments.

--- 01-run-demo.sh
+++ 01-run-demo.sh
@@ -20,16 +20,21 @@ run() {
 msg
 msg run demo:
 msg
-run kubectl run demo --image=udhos/myhello --port=9999 --labels app=demo
+run 'DEPRECATED: kubectl run demo --image=udhos/myhello --port=9999 --labels app=demo'
+msg
+run kubectl run --generator=run-pod/v1 demo --image=udhos/myhello --port=9999 --labels app=demo
 msg
 run kubectl get pods --selector app=demo
 msg
-run kubectl port-forward deploy/demo 9999:8888 --address 0.0.0.0
+run '"kubectl run" is DEPRECATED for deployments: kubectl port-forward deploy/demo 9999:8888 --address 0.0.0.0'
+msg
+run kubectl port-forward pods/demo 9999:8888 --address 0.0.0.0
 msg
 run curl localhost:9999
 msg
-run kubectl delete deployments/demo
-
+run '"kubectl run" is DEPRECATED for deployments: kubectl delete deployments/demo'
+msg
+run kubectl delete pods/demo
 msg
 msg hit ENTER to continue
 read i
