fbfd32e14280d8fbb5bc2c7aef5c7e06535a8905
udhos <udhos>
2019-08-08 18:14:24 -0300

    helm.

---
+++ 03-helm.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+me=$(basename "$0")
+
+msg() {
+    echo >&2 "$me:" "$@"
+}
+
+die() {
+	msg "$@"
+	exit 1
+}
+
+run() {
+        msg run: "$@"
+}
+
+msg
+msg intall helm, run tiller, then check:
+msg
+run helm version
+msg
+msg deploy demo using helm:
+msg
+run helm install --name demo lib/demo-helm
+msg
+msg helm terms:
+msg  chart: package containing all resource definitions
+msg  repository: place where charts can be collected and shared
+msg  release: instance of a chart running in a cluster
+msg           each release has a unique name which you specify
+msg            with the --name flag to helm install
+msg
+run helm list
+msg
+run helm status demo
+msg
+msg search helm charts - public helm charts: https://github.com/helm/charts
+msg
+run helm search redis
+msg
+run 'helm search ;# list all charts'
+msg
+msg hit ENTER to continue
+read i
+
