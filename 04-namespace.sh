303ef26b524f23d5ff7477e2ed07aa89ab9ddd4f
udhos <udhos>
2019-08-12 16:08:36 -0300

    Namespace.

---
+++ 04-namespace.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+me=$(basename "$0")
+
+msg() {
+    echo >&2 "$me:" "$@"
+}
+
+die() {
+	msg "$@"
+	exit 1
+}
+
+run() {
+        msg run: "$@"
+}
+
+msg run app 'demo' in a namespace 'demo'
+msg
+msg create namespace 'demo':
+msg
+run kubectl apply -f lib/demo-namespace/namespace.yaml
+msg
+msg create resources on namespace 'demo':
+msg
+run kubectl apply -f lib/demo-namespace -n demo
+msg
+run kubectl get namespaces
+msg
+run kubectl get all -n demo
+msg
+msg CAUTION: deleting a namespace deletes all its resources!!!
+msg
+run kubectl delete -f lib/demo-namespace/namespace.yaml
+msg OR
+run kubectl delete namespace demo
+msg
+msg hit ENTER to continue
+read i
+
